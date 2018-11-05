17e07d4ae8454774f23e3113e8c18ae42daa948d
unknown <ci.gonzalez10@uniandes.edu.co>
2018-11-05 17:22:35 -0500

    New loss function added

--- functions.py
+++ functions.py
@@ -8,11 +8,11 @@ def dice_loss_RCF(prediction, label):
     smooth = 1.
     iflat = prediction.view(-1) 
     tflat = label.view(-1) 
+
+    # Ignore 2-labels 
     mask = tflat.lt(2)
     iflat = torch.masked_select(iflat, mask)
     tflat = torch.masked_select(tflat, mask)
-
-    pdb.set_trace()
     intersection = (iflat * tflat).sum()
     return 1 - ((2. * intersection + smooth) / (iflat.sum() + tflat.sum() + smooth))
 
